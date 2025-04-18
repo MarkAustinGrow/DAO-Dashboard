42f07955ca33fbf17e4980eeff71505b5b53ea56
mark austin <mark.austin@growsudio.io>
2025-04-18 23:19:22 +0100

    Simplify theme-provider to remove next-themes dependency

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+
+// Simplified ThemeProvider that doesn't rely on next-themes
+interface ThemeProviderProps {
+  children: React.ReactNode
+  [key: string]: any
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <>{children}</>
 }
